b50a5f4b47c59ec5d61078a39487d541357f25ee
Bharati <n.bharati2@gmail.com>
2026-03-16 15:12:01 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -11,3 +11,16 @@ else:
     else:
         print("Prime")
     
+#Write a Python function called `is_prime(n)` that takes an integer `n` and returns `True` if the number is prime, and `False` otherwise.
+def is_prime(n):
+    if n < 2:
+        return False
+        
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+    
+print(is_prime(37))
+            
